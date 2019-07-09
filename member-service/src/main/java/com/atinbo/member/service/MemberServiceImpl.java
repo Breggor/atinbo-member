74d9c89e69490cd176b266a9ed1af8dcd858839d
chenlujia <chenlujia_123@163.com>
2019-07-09 20:36:36 +0800

    会员接口定义

--- member-service/src/main/java/com/atinbo/member/service/MemberServiceImpl.java
+++ member-service/src/main/java/com/atinbo/member/service/MemberServiceImpl.java
@@ -1,99 +1,37 @@
 package com.atinbo.member.service;
 
-import com.alipay.sofa.runtime.api.annotation.SofaService;
-import com.alipay.sofa.runtime.api.annotation.SofaServiceBinding;
-import com.atinbo.member.model.UserParam;
-import com.atinbo.member.model.UserBO;
-import org.springframework.stereotype.Service;
+import com.alibaba.dubbo.config.annotation.Service;
+import com.atinbo.member.model.MemberBO;
+import com.atinbo.member.model.MemberParam;
 
 import java.util.List;
 
 
 @Service
-@SofaService(interfaceType = IMemberService.class, uniqueId = "${service.unique.id}", bindings = {@SofaServiceBinding(bindingType = "bolt")})
 public class MemberServiceImpl implements IMemberService {
+
     @Override
-    public String findPhone(Long userId, String wxUnionId) {
+    public MemberBO findOne(Long memberId) {
         return null;
     }
 
     @Override
-    public boolean checkLoginNamePassword(String loginName, String password) {
+    public List<MemberBO> findAll(MemberParam param) {
+        return null;
+    }
+
+    @Override
+    public MemberBO create(MemberParam param) {
+        return null;
+    }
+
+    @Override
+    public MemberBO edit(MemberParam param) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
         return false;
-    }
-
-    @Override
-    public UserBO findByUsername(String username) {
-        return null;
-    }
-
-    @Override
-    public UserBO findByUserId(Long userId) {
-        return null;
-    }
-
-    @Override
-    public UserBO findByWxUnionId(String wxUnionId) {
-        return null;
-    }
-
-    @Override
-    public int checkVerifyCode(String phone, Integer type) {
-        return 0;
-    }
-
-    @Override
-    public List<Long> findUserIdsByGlobalId(String globalId) {
-        return null;
-    }
-
-    @Override
-    public String generateCode(String phone) {
-        return null;
-    }
-
-    @Override
-    public boolean validate(String phone, String code) {
-        return false;
-    }
-
-    @Override
-    public void updateUserInfo(String ids) {
-
-    }
-
-    @Override
-    public UserBO addUser(UserParam userParam) {
-        return null;
-    }
-
-    @Override
-    public void updateUnionId(Long userId, String wxUnionId) {
-
-    }
-
-    @Override
-    public void bindphone(Long userId, String phone) {
-
-    }
-
-    @Override
-    public void updatePassword(UserBO userVO) {
-
-    }
-
-    @Override
-    public void updateUserByUserId(Long userId, String deviceId, int platform, String channel) {
-
-    }
-
-    @Override
-    public void deleteUserByUserId(Long userId) {
-
-    }
-
-    @Override
-    public Long registerAppUser(String version, int platform, String phone, String userSource) {
-        return null;
     }
 }

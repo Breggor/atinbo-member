2488057791141690b1f074c9ad7d1ed3d7cd5b0e
Breggor <breggor@163.com>
2019-07-16 15:00:56 +0800

    init

--- member-service/src/main/java/com/atinbo/member/MemberServiceApplication.java
+++ member-service/src/main/java/com/atinbo/member/MemberServiceApplication.java
@@ -15,20 +15,7 @@ import java.io.IOException;
 @SpringBootApplication
 @EnableTransactionManagement
 public class MemberServiceApplication {
-
-
-    @Autowired
-    private Registry registry;
-
-    @PostConstruct
-    protected void init() {
-        Counter counter = registry.counter(registry.createId("http_requests_total").withTag("instant", NetworkUtil.getLocalAddress().getHostName()));
-        counter.inc();
-    }
-
     public static void main(String[] args) {
-
-
         SpringApplication.run(MemberServiceApplication.class, args);
         try {
             System.in.read();
